547fa580cd31c5ad2137cfbc0227491b5393977c
pythongit <nayak4nayak4@gmail.com>
2023-05-28 20:42:50 +0530

    changes in the web cam app

--- web_cam.py
+++ web_cam.py
@@ -1,16 +1,19 @@
 import streamlit as st
 from PIL import Image
 
+st.subheader("Color to greyscale convertor")
+
+upload_img=st.file_uploader("Upload Image")
+
 with st.expander("start camera"):
-    # start the camera
-    cam_img = st.camera_input("camera")
+    cam_img = st.camera_input("camera")   # start the camera
 
 if cam_img:
-    # create the pillow image instance
-    img = Image.open(cam_img)
+    img = Image.open(cam_img)             # create the pillow image instance
+    grey_img = img.convert("L")           # convert pillow image to greyscale
+    st.image(grey_img)                    # display greyscale image on webpage
 
-    # convert pillow image to greyscale
-    grey_img = img.convert("L")
-
-    # display greyscale image on webpage
-    st.image(grey_img)
+if upload_img:
+    img1=Image.open(upload_img)
+    grey_img1=img1.convert("L")
+    st.image(grey_img1)
